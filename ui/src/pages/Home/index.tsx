9bf23565f07812cf85bb113555bba8136cff12dd
Namson <namsonpham@tamu.edu>
2023-11-11 14:57:52 -0600

    death

--- ui/src/pages/Home/index.tsx
+++ ui/src/pages/Home/index.tsx
@@ -21,7 +21,7 @@ const Home = () => {
         title={IntroContent.title}
         content={IntroContent.text}
         button={IntroContent.button}
-        icon="developer.svg"
+        icon="pepesad.svg"
         id="intro"
       />
       <MiddleBlock
@@ -34,21 +34,21 @@ const Home = () => {
         title={AboutContent.title}
         content={AboutContent.text}
         section={AboutContent.section}
-        icon="graphs.svg"
+        icon="gottagofast.svg"
         id="about"
       />
       <ContentBlock
         direction="right"
         title={MissionContent.title}
         content={MissionContent.text}
-        icon="product-launch.svg"
+        icon="tothemoon.svg"
         id="mission"
       />
       <ContentBlock
         direction="left"
         title={ProductContent.title}
         content={ProductContent.text}
-        icon="waving.svg"
+        icon="widepeepohappy.svg"
         id="product"
       />
       <Contact
