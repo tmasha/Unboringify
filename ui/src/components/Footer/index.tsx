ce749e156a756f46bb63abb7f0e991284cc29354
Namson <namsonpham@tamu.edu>
2023-11-11 14:44:16 -0600

    landing page

--- ui/src/components/Footer/index.tsx
+++ ui/src/components/Footer/index.tsx
@@ -75,9 +75,9 @@ const Footer = ({ t }: { t: TFunction }) => {
             <Col lg={10} md={10} sm={12} xs={12}>
               <Empty />
               <Language>{t("Address")}</Language>
-              <Para>Rancho Santa Margarita</Para>
-              <Para>2131 Elk Street</Para>
-              <Para>California</Para>
+              <Para>College Station</Para>
+              <Para>400 Bizzell St</Para>
+              <Para>Texas</Para>
             </Col>
             <Col lg={8} md={8} sm={12} xs={12}>
               <Title>{t("Company")}</Title>
